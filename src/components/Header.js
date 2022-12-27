f5b9df1a5b3c56428581d8004bccf777af8e029f
YSantanas <44857937+YSantanas@users.noreply.github.com>
2022-12-26 23:46:02 -0600

    agregando carrusel en el encabezado

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,57 @@
 import React from 'react';
-import Arbol from '../assets/logoArbol.png';
+
+import Carousel from 'react-bootstrap/Carousel';
+
+
+import Galeria1 from '../assets/carrusel/galeria1.png';
+import Galeria2 from '../assets/carrusel/galeria2.png';
+import Galeria3 from '../assets/carrusel/galeria3.png';
+
+
 
 const Header = () => {
   return (
-    <div class="d-flex justify-content-center gap-5 pb-6 bg-light">
+    <div  expand="lg">
      
 
 
-    <img  src={Arbol} class="d-flex p-2" alt="logo" />
-  
+     <Carousel fade>
+      <Carousel.Item>
+        <img
+          className="d-block mx-auto img-fluid"
+          src={Galeria1}
+          alt="First slide"
+        />
+        <Carousel.Caption class="d-none d-md-block text-center text-white  bg-success bg-gradient pt-2 pb-1 opacity-50" >
+          <h3>La mejor educación a tu alcance</h3>
+          <p>  .</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block mx-auto img-fluid"
+          src={Galeria2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption class="d-none d-md-block text-center text-white bg-success bg-gradient pt-2 pb-1 opacity-50">
+          <h3>Los mejores Profesores</h3>
+          <p>  .</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block mx-auto img-fluid"
+          src={Galeria3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption class="d-none d-md-block text-center text-white bg-success bg-gradient pt-2 pb-1 opacity-50">
+          <h3>Encuentra tu carrera</h3>
+          <p>  .</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
     </div>
   );
 }
