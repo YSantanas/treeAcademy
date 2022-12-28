3ecb701962d39527b7d858c6b95a4287125da687
YSantanas <44857937+YSantanas@users.noreply.github.com>
2022-12-28 02:39:08 -0600

    Update inicio.js
    se anexa el acerca de...

--- src/paginas/Inicio.js
+++ src/paginas/Inicio.js
@@ -1,26 +1,44 @@
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
-import Galeria1 from '../assets/carrusel/galeria1.png';
+import Arbol from "../assets/logoArbol.png";
 
 const Inicio = () => {
   return (
+    <div
+      class="container justify-content-center "
+      style={{ minHeight: "80vh", paddingTop: "2vh",textAlign:"center" }}
+    >
+      
+      <h1 className="text-center text-info">Quiénes Somos</h1>
+      <Card class="container justify-content-center"  style={{ width: "50", alignItems:"center" }}>
+        <Card.Img
+          variant="top"
+          src={Arbol}
 
+          style={{ width: "20%", margin: "4px"}}
+        />
+        <Card.Body>
+          <Card.Title>¿Porqué la necesidad de crear TreeAcademy?</Card.Title>
+          <p class="text-start text-break text-wrap fs-6">
+          
+            Esta idea surgió de la realización de un proyecto academico, pronto apareció la imagen que seria
+            nuestro simbólo y la que podran observar a lo largo de la web. 
 
-<div class="container justify-content-center"  style={{ minHeight: '100vh', paddingTop: '3vh', marginTop: '3vh' }}>
-<Card style={{ width: 'auto' }}>
-      <Card.Img variant="top" src={Galeria1} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
-</div>
-
-);
+          </p>
+            <p class="text-start text-break text-wrap fs-6">
+            El significado del árbol no es más que 
+            evocar el conocimiento, un arbol esta compuesto por sus raices que vendría siendo nuestro conocimiento base,
+            pero pronto ese conocimiento base va creciendo y surgen las ramas que son precisamente esos cursos que aqui albergamos.
+            </p>
+            <p class="text-start text-break text-wrap fs-6">
+            TreeAcademy pretende encontrar la mejor educación al alcance de todos, el conocimiento crece y crede hasta hacer un 
+            inmenso arbol de conocimiento, que nos apoyará en el futuro para encontrar mejores ofertas laborales o academicas.
+            </p>
+          
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 export default Inicio;
