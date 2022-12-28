f240478e5bf473a33eabd15c73e0123e7f049e4f
YSantanas <44857937+YSantanas@users.noreply.github.com>
2022-12-28 02:04:35 -0600

    añadir catalogo de cursos

--- src/paginas/Inicio.js
+++ src/paginas/Inicio.js
@@ -1,42 +1,26 @@
-import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 import Galeria1 from '../assets/carrusel/galeria1.png';
-import Galeria2 from '../assets/carrusel/galeria2.png';
-import Galeria3 from '../assets/carrusel/galeria3.png';
 
 const Inicio = () => {
   return (
-    <div
-      className=" container w-75 border justify-content-center alert alert-light"
-      style={{ minHeight: 'auto', paddingTop: '2vh', marginTop: '5vh' }}
-    >
-      <Carousel fade>
-        <Carousel.Item>
-          <img className="d-block w-100" src={Galeria1} alt="First slide" />
-          <Carousel.Caption className="d-none d-md-block text-center text-white  bg-success bg-gradient pt-2 pb-1 opacity-50">
-            <h3>La mejor educación a tu alcance</h3>
-            <p> .</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={Galeria2} alt="Second slide" />
 
-          <Carousel.Caption className="d-none d-md-block text-center text-white bg-success bg-gradient pt-2 pb-1 opacity-50">
-            <h3>Los mejores Profesores</h3>
-            <p> .</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img className="d-block w-100" src={Galeria3} alt="Third slide" />
 
-          <Carousel.Caption className="d-none d-md-block text-center text-white bg-success bg-gradient pt-2 pb-1 opacity-50">
-            <h3>Encuentra tu carrera</h3>
-            <p> .</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    </div>
-  );
+<div class="container justify-content-center"  style={{ minHeight: '100vh', paddingTop: '3vh', marginTop: '3vh' }}>
+<Card style={{ width: 'auto' }}>
+      <Card.Img variant="top" src={Galeria1} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+</div>
+
+);
 };
 export default Inicio;
